bbc6c39cd42202baf1a9c993735ab7bc2527cc2d
Nhan Nguyen <nhanpublic@gmail.com>
2021-09-02 19:16:05 +0700

    Added cart item component

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -1,13 +1,27 @@
 import React from "react";
+import { connect, ConnectedProps } from "react-redux";
+import { RootState } from "../../redux/store";
+import CartItemCmp from "../cart-item/cart-item.component";
 import CustomButton from "../custom-button/custom-button.component";
 
 import './cart-dropdown.styles.scss'
 
-const CartDropdown = () => (
-    <div className='cart-dropdown'>
-        <div className='cart-items'></div>
-        <CustomButton>GO TO CHECKOUT</CustomButton>
-    </div>
-)
+const mapStateToProps = (state: RootState) => ({cartItems: state.cart.cartItems})
+const connector = connect(mapStateToProps)
+type PropsFromRedux = ConnectedProps<typeof connector>
 
-export default CartDropdown
+interface CartDropdownProps extends PropsFromRedux {}
+
+const CartDropdown = ({ cartItems }: CartDropdownProps) => (
+  <div className="cart-dropdown">
+    <div className="cart-items">
+      {cartItems.map((item) => (
+        <CartItemCmp key={item.id} item={item} />
+      ))}
+    </div>
+
+    <CustomButton>GO TO CHECKOUT</CustomButton>
+  </div>
+);
+
+export default connector(CartDropdown)
