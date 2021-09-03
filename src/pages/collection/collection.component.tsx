2c0ce55285dadadcebe78979a4978e53c20e3bbd
Nhan Nguyen <nhanpublic@gmail.com>
2021-09-03 22:43:13 +0700

    Added basic collection page

--- src/pages/collection/collection.component.tsx
+++ src/pages/collection/collection.component.tsx
@@ -1,12 +1,30 @@
 import React from "react";
+import { connect, ConnectedProps } from "react-redux";
 import { RouteComponentProps } from "react-router-dom";
+import { selectOneCollection } from "../../redux/collection/collection.selectors";
+import { RootState } from "../../redux/store";
 
 import './collection.styles.scss'
 
-const CollectionPage = ({match}: RouteComponentProps) => (
+interface RouteParams {
+    collectionId: string
+}
+
+const mapStateToProps = (state: RootState, ownProps: RouteComponentProps<RouteParams>) => ({
+    collection: selectOneCollection(ownProps?.match?.params?.collectionId)(state)
+})
+
+const connector = connect(mapStateToProps)
+
+type PropsFromRedux = ConnectedProps<typeof connector> & RouteComponentProps<RouteParams>
+
+const CollectionPage = ({match, collection}: PropsFromRedux) => (
     <div className='category'>
-        <h2>COLLECTION PAGE</h2>
+        <h2>{collection?.title}</h2>
+        {
+            collection?.items.map(item => <h3>{item.name}</h3>)
+        }
     </div>
 )
 
-export default CollectionPage
+export default connector(CollectionPage)
--- src/redux/collection/collection.selectors.ts
+++ src/redux/collection/collection.selectors.ts
@@ -1,6 +1,20 @@
 import { createSelector } from "reselect";
 import { RootState } from "../store";
 
+const COLLECTION_ID_MAP: {[key: string]: number} = {
+    hats: 1,
+    sneakers: 2, 
+    jackets: 3,
+    womens: 4,
+    mens: 5,
+}
+
 const selectCollection = (state: RootState) => state.collection
 
 export const selectCollections = createSelector([selectCollection], collection => collection.collections)
+
+export const selectOneCollection = (collectionSlug: string) => 
+    createSelector(
+        [selectCollections], 
+        collections => collections.find(c => c.id === COLLECTION_ID_MAP[collectionSlug])
+    )
